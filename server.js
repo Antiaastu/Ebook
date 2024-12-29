8f073dbcca2c8776ff8a31735c07d0d3241c0c19
Antiaastu <136442178+Antiaastu@users.noreply.github.com>
2024-12-29 13:15:39 +0300

    fifth commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors()); 
 
-app.use('/uploads', cors(), express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Database connection
 connectDB();
