5860b6920dd29a414d9da48ccef65dd8f98a776b
Antiaastu <136442178+Antiaastu@users.noreply.github.com>
2024-12-29 13:09:19 +0300

    fifth commit

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors()); 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/uploads', cors(), express.static(path.join(__dirname, 'uploads')));
 
 // Database connection
 connectDB();
